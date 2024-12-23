47cfa24652fdb7d5a32fc1f00dba005af2d23b9e
husna510 <husnanazeer30@gmail.com>
2024-12-23 23:24:00 +0500

    Fixed the deployement issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import NavBar from "@/components/navBar";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
